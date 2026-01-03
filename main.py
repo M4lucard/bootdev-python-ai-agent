7fd26aa7aeb1f01593ae3649d2a558768349cd61
M4lucard <woutdeceuninck@hotmail.be>
2026-01-03 17:22:56 +0100

    Post LLM chapter

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-
-
+import argparse
+from google.genai import types
 
 def main():
-    print("Hello from ai-agent!")
-    load_dotenv()
 
+    load_dotenv() 
 
     api_key =  os.environ.get("GEMINI_API_KEY")
     if api_key == None:
@@ -17,18 +16,27 @@ def main():
         client = genai.Client(api_key=api_key)
         print("client made")
 
-    content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    response = client.models.generate_content(model='gemini-2.5-flash', contents=content)
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose",action="store_true",help="Enable verbose output")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+
+    request = args.user_prompt
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = client.models.generate_content(model='gemini-2.5-flash', contents=messages)
     
-    print(f"User prompt: {content}")
+    if args.verbose:
+        print(f"User prompt: {request}")
+        if response.usage_metadata == None:
+            raise RuntimeError("response Metadata empty")
+        else:
+            # number of tokens in the prompt that was sent to the model:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            # number of tokens in the model's response:
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     
-    if response.usage_metadata == None:
-        raise RuntimeError("response Metadata empty")
-    else:
-        # number of tokens in the prompt that was sent to the model:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        # number of tokens in the model's response:
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
 
     print("Response:")
     print(response.text)
