5023e9b2540ba93c570982aa2651515231e5c49e
M4lucard <woutdeceuninck@hotmail.be>
2026-01-05 20:40:36 +0100

    post calculator

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,15 +2,32 @@ import os
 
 
 
-
 def get_files_info(working_directory, directory="."):
-    working_dir_abs = os.path.abspath(working_directory)
-    print(f"absolute path of working directory: {working_dir_abs}")
-    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
-    print(f"Target dir, joined abs/directory: {target_dir}")
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
 
-    # Will be True or False
-    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        # Will be True or False
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
 
-    if not valid_target_dir:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        
+        files = os.listdir(target_dir)
+
+        lines = []
+
+        for file in files:
+            full_file_path = os.path.join(target_dir, file)
+
+            size = os.path.getsize(full_file_path)
+            is_dir = os.path.isdir(full_file_path)
+            lines.append(f'- {file}: file_size={size} bytes, is_dir={is_dir}') 
+            
+
+        return "\n".join(lines)
+    except Exception as e:
+        return f"Error: {e}"
+
